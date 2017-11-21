78a91ef8e7f76334fcd3f8f8bf9d21f4f64f55e8
sarah <sarahslee@berkeley.edu>
2017-11-21 09:03:12 -0800

    final

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,48 @@
 $(document).ready(function() {
 
-	setTimeout(function() {$("#aboutbox").fadeIn();}, 5000);
-
-	$("navbaritem").hover(function(){
-		$("dropdown").hasClass("inactive")
+	$("#adventures").hover(function(){
+		if ($("#adventuredrop").hasClass("active")) {
+			$("#adventuredrop").removeClass("active");
+			setTimeout(function(){
+				$("#adventuredrop").hide();
+			}, 500);
+		} else {
+			$("#adventuredrop").show();
+			setTimeout(function()
+				{$("#adventuredrop").addClass("active");
+			}, 0);
+		}	
 	})
+
+	$("#projects").hover(function(){
+		if ($("#projectdrop").hasClass("active")) {
+			$("#projectdrop").removeClass("active");
+			setTimeout(function(){
+				$("#projectdrop").hide();
+			}, 500);
+		} else {
+			$("#projectdrop").show();
+			setTimeout(function()
+				{$("#projectdrop").addClass("active");
+			}, 0);
+		}	
+	})
+
+/*	
+	$("#design").hover(function(){
+		if ($("#designdrop").hasClass("active")) {
+			$("#designdrop").removeClass("active");
+			setTimeout(function(){
+				$("#designdrop").hide();
+			}, 500);
+		} else {
+			$("#designdrop").show();
+			setTimeout(function()
+				{$("#designdrop").addClass("active");
+			}, 0);
+		}	
+	})
+*/
+	setTimeout(function() {$("#aboutbox").fadeIn();}, 8000);
+
 });
